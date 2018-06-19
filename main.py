1f6c64378bf06b4caa7f707216e513511312054e
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-06-19 10:05:23 -0500

    Added instaby client configurations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from plugins import bitly_plugin, hootsuite_plugin, imgur_plugin, instapy_plugin, unsplash_plugin, pexels_plugin
+from plugins import bitly_plugin, hootsuite_plugin, instapy_plugin, unsplash_plugin, pexels_plugin
 from credentials import config
 
 import multiprocessing
@@ -12,4 +12,14 @@ configs = config.configurations
 unsplash = unsplash_plugin.UnsplashClient(configs["api_keys"]["UNSPLASH_API_KEY"])
 pexels = pexels_plugin.PexelsClient(configs["api_keys"]["PEXELS_API_KEY"])
 
-print(unsplash.get_images(10, "office"))
+instapy = instapy_plugin.InstapyClient(
+    configs["instapy_configs"]["INSTAGRAM_USERNAME"],
+    configs["instapy_configs"]["INSTAGRAM_PASSWORD"],
+    configs["instapy_configs"]["NICHES"],
+    configs["instapy_configs"]["COMMENTS"],
+    configs["instapy_configs"]["COMMENT_PERCENTAGE"],
+    configs["instapy_configs"]["LIKE_PRECENTAGE"]
+)
+
+if __name__ == '__main__':
+    instapy.like()
