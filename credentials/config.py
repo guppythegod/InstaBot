478201fda1382c6517a8ce584628c7d789a86aef
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-06-29 00:57:28 -0500

    Added more configs

--- credentials/config.py
+++ credentials/config.py
@@ -1,15 +1,23 @@
 configurations = {
     "api_keys" : {
         "UNSPLASH_API_KEY" : "b7d9b800c727d359fdf0af24aca9867c5700a6ed1ec8fc26142bcd4e887095db",
-        "PEXELS_API_KEY": "",
-    }
-    "instapy_config" : {
+        "PEXELS_API_KEY": "563492ad6f91700001000001f8da2d83bbbf46819af7f80109a9f22e",
+    },
+    "instapy_configs" : {
         "INSTAGRAM_USERNAME" : "",
         "INSTAGRAM_PASSWORD" : "",
         "NICHES" : [
             "",
             "",
             "",
-        ]
+        ],
+        "COMMENTS" : [
+            "",
+            "",
+            "",
+        ],
+        "COMMENT_PERCENTAGE" : 50,
+        "LIKE_PERCENTAGE" : 75,
+        
     }
 }
