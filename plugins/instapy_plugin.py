0e9c99d171ea69320bd84f932769a4f0de531d1a
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-06-29 00:57:45 -0500

    Added more to Instapy to fix some problems

--- plugins/instapy_plugin.py
+++ plugins/instapy_plugin.py
@@ -1,21 +1,20 @@
-from instapy import Instapy
+from instapy import instapy
 
 class InstapyClient:
-
     def __init__(self, username, password, niches, comments, comment_percentage, like_percentage):
         self.username  = username
         self.password = password
         self.niches = niches
         self.like_percentage = like_percentage
-        session = Instapy(username=self.username,password=self.password)
+        session = instapy(username=self.username,password=self.password,nogui=True)
         session.login()
         session.set_do_comments(True, percentage=comment_percentage)
         session.set_comments(comments)
-        session.set_do_like = (enabled=True, percentage=self.like_percentage)
+        session.set_do_like(enabled=True, percentage=self.like_percentage)
     
-    def like_and_comment():
-        session.like_by_tags(self.niche)
-
+    def like():
+        session.like_by_tags(self.niche, amount=500)
+        
     def follow():
         pass
     
